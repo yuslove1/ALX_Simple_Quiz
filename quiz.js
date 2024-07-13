9f1bfc7bc7efe4c15d48abe4ff883f6246276288
yuslove1 <adexyuslove@gmail.com>
2024-07-13 00:59:55 -0700

    correcting the implicit camparison to explicit

--- quiz.js
+++ quiz.js
@@ -4,7 +4,7 @@ function checkAnswer(){
     const userChoice = document.querySelector('input[name="quiz"]:checked');
     const feedback = document.getElementById('feedback')
     let userAnswer = userChoice? userChoice.value : null;
-    if(userAnswer == correctAnswer){
+    if(userAnswer === correctAnswer){
       feedback.textContent = "Correct! Well done";
     }else{
        feedback.textContent = "That's incorrect. Try again!"
