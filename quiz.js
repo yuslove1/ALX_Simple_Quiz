412f40ff24fb728c75a31942dcde2b54a1084f5b
yuslove1 <adexyuslove@gmail.com>
2024-07-13 01:07:50 -0700

    correcting the implicit camparison to explicit

--- quiz.js
+++ quiz.js
@@ -2,12 +2,13 @@
 function checkAnswer(){
     const correctAnswer = "4";
     const userChoice = document.querySelector('input[name="quiz"]:checked');
-    const feedback = document.getElementById('feedback')
+    const feedback = document.getElementById('feedback');
     let userAnswer = userChoice? userChoice.value : null;
-    if(userAnswer === correctAnswer){
+
+    if (userAnswer === correctAnswer){
       feedback.textContent = "Correct! Well done";
     }else{
-       feedback.textContent = "That's incorrect. Try again!"
+       feedback.textContent = "That's incorrect. Try again!";
     }
 }
 
