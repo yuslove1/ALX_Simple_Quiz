98bda252d640c40b44921a75dc9435eacfed67e3
yuslove1 <adexyuslove@gmail.com>
2024-07-13 00:48:32 -0700

    task 0: Implement a simple interactive Quiz

---
+++ quiz.js
@@ -0,0 +1,15 @@
+
+function checkAnswer(){
+    const correctAnswer = "4";
+    const userChoice = document.querySelector('input[name="quiz"]:checked');
+    const feedback = document.getElementById('feedback')
+    let userAnswer = userChoice? userChoice.value : null;
+    if(userAnswer == correctAnswer){
+      feedback.textContent = "Correct! Well done";
+    }else{
+       feedback.textContent = "That's incorrect. Try again!"
+    }
+}
+
+const btn = document.getElementById('submit-answer');
+btn.addEventListener('click', checkAnswer);
